fb860b6345ea8e3b58ebe886739a69da47b8fd27
roshen1234 <roshankoshy123@gmail.com>
2024-01-05 12:26:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
+import Homes from './pages/Homes';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (<Homes/>),
+  },
+  {
+    path: "/login",
+    element: (<LoginPage/>),
+  },
+  {
+    path: "/signup",
+    element:(<SignupPage/>),
+  },
+]);
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+       <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logins from '../features/auth/components/Logins'
+
+const LoginPage = () => {
+  return (
+    <div>
+      <Logins/>
+    </div>
+  )
+}
+
+export default LoginPage
