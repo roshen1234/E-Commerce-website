60719b2d5e1f3be79c76db823502ccf2fb538de6
roshen1234 <roshankoshy123@gmail.com>
2024-01-06 17:49:30 +0530

    shows item accoring to category and brand selected and also made sort option functional commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import productReducer from '../features/product-list/productListSlice';
 import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    product: productReducer,
+    counter: counterReducer
   },
 });
